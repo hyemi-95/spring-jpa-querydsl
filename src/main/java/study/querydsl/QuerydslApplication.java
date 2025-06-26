82ba63a712034973aa12be722835a9407b45d30d
Kim Hyemi <khm24ks@naver.com>
2025-06-26 22:18:14 +0900

    순수JPA와 Querydsl > 리포지토리, 동적쿼리 최적화(빌더, 조건 파라미터), API컨트롤러 생성

--- src/main/java/study/querydsl/QuerydslApplication.java
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -1,13 +1,21 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuerydslApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuerydslApplication.class, args);
+	}
+
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em) { //동시성 문제 없이 트랜잭션 단위로 따로따로 분리되서 동작함
+		return new JPAQueryFactory(em);
 	}
 
 }
